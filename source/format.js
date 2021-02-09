390a6311aae9622d82a0de0fd4de7067d36948bc
Andrew <a-kireev1989@mail.ru>
2021-02-09 18:45:04 +0300

    some corrections

--- source/format.js
+++ source/format.js
@@ -1,5 +1,24 @@
 'use strict';
 
+
+/***
+  * The function find last element for every columns 
+  *
+  * @param {Array} rows - rows
+  * @param {Number} columns - number of colums
+  * @returns {String}
+  */
+const findEndLine = (rows, columns) => {
+    let endLine = new Array(columns)
+    for (let i = 0; i < columns; i++) {
+        if (rows[rows.length - 1].length > i)
+            endLine[i] = rows[rows.length - 1][i]
+        else
+            endLine[i] = rows[rows.length - 2][i]
+    }
+    return endLine
+}
+
 /***
   * The function formats the given numbers into several columns 
   *
@@ -7,11 +26,28 @@
   * @param {Number} columns - number of colums
   * @returns {String}
   */
-
 const format = (array, columns) => {
-    const rows = [...Array(Math.ceil(array.length / columns))].map((item, index) => array.map(item => String(item)).slice(index * columns, (index + 1) * columns));
-    const cols = [...Array(columns)].map((_, index) => array.map(item => String(item)).filter((n, i) => i % columns === index))
+    if (columns < 0)
+        return null;
+    const rows = Array(Math.ceil(array.length / columns)).fill(0).map((item, index) => array.map(item => String(item)).slice(index * columns, (index + 1) * columns));
+    const cols = Array(columns).fill(0).map((_, index) => array.map(item => String(item)).filter((n, i) => i % columns === index))
     const widths = cols.map((item, index) => Math.max(...item.map(i => i.length)))
+    
+    let endLine = findEndLine(rows, columns)
+    let finalResult = ''
+    console.log(endLine)
 
-    return rows.map(n => n.map((m, i) => m.padStart(widths[i], ' ')).join(' ')).join('\n');
+    rows.forEach(function (item, index){
+        item.forEach(function (item2, index2) {
+            let repeat = endLine[index2].length - item2.length
+            if (columns != 1 && index2 % columns != 0)
+                repeat++
+            if (repeat > 0)
+                finalResult += ' '.repeat(repeat)
+            finalResult += item2
+        });
+        if (index != rows.length - 1)
+            finalResult += '\n'
+    });
+    return finalResult
 }
